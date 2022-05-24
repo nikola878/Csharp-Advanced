70ea9cc5135e98f0a44c881f1e6e78ceaa49d4bc
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-05-24 11:43:34 +0300

    Add files via upload

---
+++ Exercise: Stacks and Queues/FastFood/FastFood/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            List<int> listOrders = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+            Queue<int> queueOrders = new Queue<int>(listOrders);
+
+            Console.WriteLine(queueOrders.Max());
+
+            int count = queueOrders.Count;
+
+            for (int order = 1 ; order <= count; order++)
+            {
+                if (foodQuantity >= queueOrders.Peek())
+                {
+                    foodQuantity -= queueOrders.Peek();
+                    queueOrders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (queueOrders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queueOrders)}");
+            }
+        }
+    }
+}
