d982054af0fec657cf7561c33a4cd7aa78a3e7af
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-05-25 14:07:42 +0300

    Add files via upload

---
+++ MultidimensionalArrays/SumMatrixElements/SumMatrixElements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            (int rows, int cols) = (dimensions[0], dimensions[1]);
+
+            int[,] matrix = new int[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                int[] line = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+            long sum = 0;
+            for (int row = 0; row < rows; row++)
+                for (int col = 0; col < cols; col++)
+                    sum += matrix[row, col];
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+                
+            
+        }
+    }
+}
