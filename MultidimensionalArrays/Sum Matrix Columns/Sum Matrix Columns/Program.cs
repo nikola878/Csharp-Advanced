69bd17369e86a9d3acd2092723770fc6551338cd
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-02 16:51:06 +0300

    Add files via upload

---
+++ MultidimensionalArrays/Sum Matrix Columns/Sum Matrix Columns/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            (int rows, int cols) = (matrixSize[0], matrixSize[1]);
+
+            int[,] matrix = new int[rows, cols];
+
+            MatrixReading(matrix, rows, cols);
+
+            for (int col = 0; col < cols; col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+            }
+
+        }
+
+        private static void MatrixReading(int[,] matrix, int rows, int cols)
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                int[] line = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+        }
+    }
+}
