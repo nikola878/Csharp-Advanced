2bd10b9256bfec3073ae85ba7f74b7cc43938510
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-02 14:05:56 +0300

    Add files via upload

---
+++ MultidimensionalArrays/Diagonal Difference/Diagonal Difference/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] numbers = new int[n, n];
+            FillMatrix(numbers);
+
+            int sumPrimeryDiagonal = 0;
+            
+            int sumSecondaryDiagonal = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    int number = numbers[row, col];
+
+                    if (row == col)
+                    {
+                        sumPrimeryDiagonal += number;
+                    }
+
+                    if (row + col == n - 1)
+                    {
+                        sumSecondaryDiagonal += number;
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sumPrimeryDiagonal - sumSecondaryDiagonal));
+            
+        }
+
+        private static void FillMatrix(int[,] numbers)
+        {
+            for (int row = 0; row < numbers.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < numbers.GetLength(1); col++)
+                {
+                    numbers[row, col] = rowData[col];
+                }
+            }
+        }
+    }
+}
