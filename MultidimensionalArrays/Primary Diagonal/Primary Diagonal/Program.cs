25ec8b7e93fef761bdca971b4f4c5f373491e13d
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-02 17:06:36 +0300

    Add files via upload

---
+++ MultidimensionalArrays/Primary Diagonal/Primary Diagonal/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n,n];
+            FillMatrix(matrix);
+            int sum = 0;
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    int number = matrix[row, col];
+
+                    if (row == col)
+                    {
+                        sum += number;
+                    }
+                }
+            }
+            Console.WriteLine(sum);
+        }
+
+        private static void FillMatrix(int[,] numbers)
+        {
+            for (int row = 0; row < numbers.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < numbers.GetLength(1); col++)
+                {
+                    numbers[row, col] = rowData[col];
+                }
+            }
+        }
+    }
+}
