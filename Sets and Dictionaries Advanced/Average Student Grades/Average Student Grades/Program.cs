d5d09ec8de6909a00055729c193c944ea83b161c
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-03 21:03:39 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Average Student Grades/Average Student Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentsGrade = new Dictionary<string, List<decimal>>();
+            int gradesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < gradesCount; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                string name = line[0];
+                decimal grade = decimal.Parse(line[1]);
+                if (!studentsGrade.ContainsKey(name))
+                {
+                    studentsGrade.Add(name, new List<decimal>());
+                }
+                studentsGrade[name].Add(grade);
+            }
+
+            foreach (var name in studentsGrade.Keys)
+            {
+                List<decimal> grades = studentsGrade[name];
+                string gradesStr = string.Join(" ",
+                    grades.Select(g => g.ToString("f2")));
+                decimal avg = grades.Average();
+                Console.WriteLine($"{name} -> {gradesStr} (avg: {avg:f2})");
+            }
+        }
+    }
+}
