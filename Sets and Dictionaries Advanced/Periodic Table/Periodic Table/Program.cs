7f1a404124378a48924046f9df57534170ad70a6
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-05 20:11:51 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Periodic Table/Periodic Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            HashSet<string> set = new HashSet<string>();
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                foreach (var item in input)
+                {
+                    set.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', set.OrderBy(s => s)));
+        }
+    }
+}
