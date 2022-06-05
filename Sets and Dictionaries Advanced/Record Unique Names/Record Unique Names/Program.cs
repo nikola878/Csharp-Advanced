97c2e2827fba40517eaf2ac8c4360dcef237080d
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-05 18:03:44 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Record Unique Names/Record Unique Names/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+            for (int i = 0; i < lines; i++)
+            {
+                string name = Console.ReadLine();
+                
+                names.Add(name);
+            }
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
