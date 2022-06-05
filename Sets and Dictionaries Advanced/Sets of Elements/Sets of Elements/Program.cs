642f692afd5c1b60c577d4a7dd9fa60a3fccf942
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-05 18:44:28 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Sets of Elements/Sets of Elements/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int n = ints[0];
+            int m = ints[1];
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+            List<int> list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                set1.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int i = 0; i < m; i++)
+            {
+                set2.Add(int.Parse(Console.ReadLine()));
+            }
+
+            if (n >= m)
+            {
+                foreach (var item in set2)
+                {
+                    if (set1.Contains(item))
+                    {
+                        list.Add(item);
+                    }
+                }
+            }
+            else if (n < m)
+            {
+                foreach (var item in set1)
+                {
+                    if (set2.Contains(item))
+                    {
+                        list.Add(item);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', list));
+        }
+    }
+}
