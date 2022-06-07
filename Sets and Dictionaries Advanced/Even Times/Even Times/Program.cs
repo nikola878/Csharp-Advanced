85f9ad51b386e8f5e38c511dd1317e4f4723608c
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-07 19:35:05 +0300

    Add files via upload

---
+++ Sets and Dictionaries Advanced/Even Times/Even Times/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> digits = new Dictionary<int, int>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (!digits.ContainsKey(num))
+                {
+                    digits.Add(num, 1);
+                }
+                else
+                {
+                    digits[num]++;
+                }
+            }
+            int theNum = 0;
+            foreach (var num in digits)
+            {
+                if (num.Value % 2 == 0)
+                {
+                    theNum = num.Key;
+                }
+            }
+
+            Console.WriteLine(theNum);
+        }
+    }
+}
