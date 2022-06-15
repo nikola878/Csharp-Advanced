468e65eb273f7c19a13c606cfe1e78da22bc6146
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-15 13:44:44 +0300

    Add files via upload

---
+++ Functional Programming/Action Point/Action Point/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(' ');
+            for (int i = 0; i < names.Length; i++)
+            {
+                Action<string> namess = name => Console.WriteLine(name);
+                namess(names[i]);
+            }
+        }
+    }
+}
