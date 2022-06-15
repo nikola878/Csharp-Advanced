83b156826321582709ea98b9bf2e5734c9b462d3
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-15 13:48:43 +0300

    Add files via upload

---
+++ Functional Programming/Knights of Honor/Knights of Honor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(' ');
+            for (int i = 0; i < names.Length; i++)
+            {
+                Action<string> namess = name => Console.WriteLine("Sir " + name);
+                namess(names[i]);
+            }
+        }
+    }
+}
