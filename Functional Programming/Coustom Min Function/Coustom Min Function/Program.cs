5270ac1402c2227c3cb5c8911cf4e799199b0874
Nikola Hadjiev <83541964+nikola878@users.noreply.github.com>
2022-06-16 11:36:10 +0300

    Add files via upload

---
+++ Functional Programming/Coustom Min Function/Coustom Min Function/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Func<int[], int> func = numbers => numbers.Min();
+            Console.WriteLine(func(nums));
+        }
+    }
+}
